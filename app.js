63b843ce940d6f8d7abac84b461cd246d2e46147
Mike Lopez <mikeglopez.91@gmail.com>
2019-01-08 17:13:54 -0600

    Complete implementing GroceryListItem componen

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
 // TODO
+var GroceryListItem = (props) => (
+  <ul>
+    <li>{props.groceryItems[0]}</li>
+    <li>{props.groceryItems[1]}</li>
+    <li>{props.groceryItems[2]}</li>
+    <li>{props.groceryItems[3]}</li>
+    <li>{props.groceryItems[4]}</li>
+  </ul>
+);
+
 var GroceryList = () => (
-  <div>
-    <ul>
-      <Tacos />
-      <Bacon />
-    </ul>
-  </div>
-);
-
-var Tacos = () => (
-  <li>Tacos</li>
-);
-
-var Bacon = () => (
-  <li>Bacon</li>
+  <GroceryListItem groceryItems={['Tacos', 'Bacon', 'Lettuce', 'Tomato', 'Dressing']}/>
 );
 
 ReactDOM.render(<GroceryList />, document.getElementById('app'));
